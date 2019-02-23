a2675167013faffe3390fa1b6b277aab06da5c54
thanus88 <m.sanla2525@gmail.com>
2019-02-23 20:58:10 +0700

    Add files via upload

---
+++ src/test/java/com/abs/corp/services/ABS/Corp/Services/AbsCorpServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abs.corp.services.ABS.Corp.Services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbsCorpServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
